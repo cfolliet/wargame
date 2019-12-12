1e8a48af853f3471774dcbab3152ba812da3ac64
Clément Folliet <cfolliet@talentsoft.com>
2019-12-12 09:58:55 +0100

    Broadcast board updates

--- server/main.js
+++ server/main.js
@@ -9,18 +9,29 @@ const server = new WebSocket.Server({ port: 9000 });
 const clients = new Set;
 
 class Client {
-    constructor(conn, clients, board) {
-        clients.add(this);
+    constructor(conn, clients) {
+        this.clients = clients;
+        this.clients.add(this);
         this.conn = conn;
-        this.board = board
+        this.board = null;
+    }
+    join(board) {
+        this.board = board;
+        this.board.createPlayer('player' + clients.size);
+        this.send(board);
+        this.broadcast({ type: 'update-board', value: board });
+    }
+    leave() {
+        this.clients.delete(this);
+        this.broadcast({ type: 'update-board', value: board });
     }
     send(data) {
         const msg = JSON.stringify(data);
         this.conn.send(msg);
     }
-    broadcast(data){
-        [...clients].forEach(client => {
-            if(client === this){
+    broadcast(data) {
+        [...this.clients].forEach(client => {
+            if (client === this) {
                 return;
             }
 
@@ -29,7 +40,7 @@ class Client {
     }
     receive(message) {
         const data = JSON.parse(message);
-        if(data.type == 'create-bullet'){
+        if (data.type == 'create-bullet') {
             this.board.createBullet(data.value.player, data.value.vel);
             this.broadcast(data);
         }
@@ -37,16 +48,10 @@ class Client {
 }
 
 server.on('connection', conn => {
-    const client = new Client(conn, clients, board);
-    board.createPlayer('player' + clients.size);
+    const client = new Client(conn, clients);
 
-    conn.on('message', msg => {
-        client.receive(msg);
-    });
+    conn.on('message', msg => client.receive(msg));
+    conn.on('close', () => client.leave());
 
-    conn.on('close', () => {
-        clients.delete(client);
-    })
-
-    client.send(board);
+    client.join(board);
 });
