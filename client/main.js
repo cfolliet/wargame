e344ea9d8b1293ed7dc7062dd0627d18150a9989
Clément Folliet <cfolliet@talentsoft.com>
2019-12-13 17:41:51 +0100

    add zqsd keys for controlling the player

--- client/main.js
+++ client/main.js
@@ -28,32 +28,32 @@ document.addEventListener('mouseup', event => {
 });
 
 document.addEventListener("keydown", event => {
-    if (event.keyCode == 37) {
+    if (event.keyCode == 37 || event.keyCode == 81) {
         send({ type: 'move-player', value: { axis: 'x', direction: -1 } });
         board.movePlayer('x', -1);
-    } else if (event.keyCode == 38) {
+    } else if (event.keyCode == 38 || event.keyCode == 90) {
         send({ type: 'move-player', value: { axis: 'y', direction: -1 } });
         board.movePlayer('y', -1);
-    } else if (event.keyCode == 39) {
+    } else if (event.keyCode == 39 || event.keyCode == 68) {
         send({ type: 'move-player', value: { axis: 'x', direction: 1 } });
         board.movePlayer('x', 1);
-    } else if (event.keyCode == 40) {
+    } else if (event.keyCode == 40 || event.keyCode == 83) {
         send({ type: 'move-player', value: { axis: 'y', direction: 1 } });
         board.movePlayer('y', 1);
     }
 });
 
 document.addEventListener("keyup", event => {
-    if (event.keyCode == 37) {
+    if (event.keyCode == 37 || event.keyCode == 81) {
         send({ type: 'move-player', value: { axis: 'x', direction: 0 } });
         board.movePlayer('x', 0);
-    } else if (event.keyCode == 38) {
+    } else if (event.keyCode == 38 || event.keyCode == 90) {
         send({ type: 'move-player', value: { axis: 'y', direction: 0 } });
         board.movePlayer('y', 0);
-    } else if (event.keyCode == 39) {
+    } else if (event.keyCode == 39 || event.keyCode == 68) {
         send({ type: 'move-player', value: { axis: 'x', direction: 0 } });
         board.movePlayer('x', 0);
-    } else if (event.keyCode == 40) {
+    } else if (event.keyCode == 40 || event.keyCode == 83) {
         send({ type: 'move-player', value: { axis: 'y', direction: 0 } });
         board.movePlayer('y', 0);
     }
